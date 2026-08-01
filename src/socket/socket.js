b3f81eb6a3e5e7e7f77a521fd3d538b093692e9c
Arun <arunjamdagni2@gmail.com>
2026-08-01 17:30:38 +0530

    changes

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -2,7 +2,9 @@
 
 import { io } from "socket.io-client";
 
-export const socket = io(import.meta.env.VITE_API_URL ||"http://localhost:5000", {
-  reconnection:true,
-  withCredentials: true,
+export const socket = io(import.meta.env.VITE_API_URL, {
+  transports: ["websocket", "polling"],
+  reconnection: true,
+  reconnectionAttempts: Infinity,
+  reconnectionDelay: 1000,
 });
